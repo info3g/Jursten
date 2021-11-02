62e52f115f045c8c6967b232aee138a6fad7ae34
Dev Team <infothreeg@gmail.com>
2021-11-02 15:48:22 +0530

    Update Justren_code.py

--- Justren_code.py
+++ Justren_code.py
@@ -11,7 +11,10 @@ from selenium.webdriver.common.keys import Keys
 import json
 from selenium import webdriver
 from selenium.webdriver.common.proxy import *
-
+import re
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 # Source Code
 class Login:
@@ -27,21 +30,44 @@ class Login:
     # Information function
     def Infomation(self):
         print("-----------------")
-        # Open chrome web browser and headless
-        driver = webdriver.Chrome()
-        # Get Trace_id dynamically 
+        # Scraped Trace Id dynamically
         try:
-            driver.get('https://beta.scouted.by/v1/exportDeepScouting?id=d0dd6e0cf620541fdd14527cc9a9813a&signature=6jNUs1R47zOMaW8goYDxIa5XIzA=')
-            time.sleep(6)
-            ids =driver.find_element_by_xpath('//*[@style="word-wrap: break-word; white-space: pre-wrap;"]')
-            id_id = ids.text
-            print(id_id)
-            res = json.loads(id_id)
-            Trace_id = res['trace_id']
-            print('Trace_id:------------',Trace_id)
+            link = 'https://beta.scouted.by/v1/exportDeepScouting?id=d0dd6e0cf620541fdd14527cc9a9813a&signature=6jNUs1R47zOMaW8goYDxIa5XIzA='
+            f = requests.get(link)
+            print(json.loads(f.text)['trace_id'])
+            Trace_id = json.loads(f.text)['trace_id']
+            print('try Trace_id:-----------------------',Trace_id)
         except:
-            Trace_id = '617a5148421aa9600f98d653'
+            Trace_id = '617a5261421aa9600f98d65d'
             print('except:------------',Trace_id)
+
+        accounts = [('mikalouisa2','kp48ffkn'),('LianneStaps9','kp48ffkn'),('louisamika2','kp48ffkn')]
+        random.shuffle(account)    
+        Insta_user = accounts[0][0]
+        print('Insta_user:------',Insta_user)
+        link = 'https://www.instagram.com/accounts/login/'
+        login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+        time = int(datetime.now().timestamp())
+
+        payload = {
+            'username': Insta_user,
+            'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:kp48ffkn',
+            'queryParams': {},
+            'optIntoOneTap': 'false'
+        }
+        with requests.Session() as s:
+            r = s.get(link)
+            csrf = re.findall(r"csrf_token\":\"(.*?)\"", r.text)[0]
+            r = s.post(login_url, data=payload, headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+                "X-Requested-With": "XMLHttpRequest",
+                "Referer": "https://www.instagram.com/accounts/login/",
+                "x-csrftoken": csrf
+            })
+            print(r.json())
+
+
 
 
         # Instagram Login
@@ -57,7 +83,7 @@ class Login:
         list_of_user = []
         print("***********")
         for users in self.user:
-
+            time.sleep(10)
             print('users:---------------',users)
             #sent the username to fetching the information of the user.
             profile = Profile.from_username(loader.context, users)
@@ -70,6 +96,7 @@ class Login:
             for following in followings:
                 if following not in list_of_user:
                     print(following.username)
+                    time.sleep(0.30)
                     list_of_user.append(following.username)
                     row = [following.username]
                     
@@ -77,76 +104,80 @@ class Login:
             for follower in followers:
                 if follower not in list_of_user:
                     print(follower.username)
+                    time.sleep(0.30)
                     list_of_user.append(follower.username)
                     row = [follower.username]
             
             # Get information 
             def Get_info(list_of_user):
                 # Scraped user deatil one by one.
+                test = []
                 for user_name in list_of_user:
-                    Photo3 = []
                     try:
                         print("New user:--------------",user_name)
-                        profile = Profile.from_username(loader.context, user_name)
-                        # User profile picture
-                        print('profile_pic_url_hd',profile.__dict__['_node']['profile_pic_url_hd'].replace('?','----').replace('&','____'))
-                        profile_pic = profile.__dict__['_node']['profile_pic_url_hd'].replace('?','----').replace('&','____')
-                        # User followers count
-                        print('followers',profile.__dict__['_node']['edge_followed_by']['count'])
-                        followers = profile.__dict__['_node']['edge_followed_by']['count']
-                        # User following count
-                        print('Following',profile.__dict__['_node']['edge_follow']['count'])
-                        Following = profile.__dict__['_node']['edge_follow']['count']
+                        data = s.get('https://www.instagram.com/'+str(user_name)+'/?__a=1')
+                        time.sleep(8)
+                        details = json.loads(data.text)['graphql']['user']
                         # User rofile description
-                        print('description',profile.__dict__['_node']['biography'])
-                        description = profile.__dict__['_node']['biography']
+                        time.sleep(1)
+                        description = details['biography']
+                        # User followers count
+                        time.sleep(1)
+                        followers = details['edge_followed_by']['count']
+                        # User Following count
+                        time.sleep(1)
+                        Following = details['edge_follow']['count']
+                        # User profile picture
+                        time.sleep(1)
+                        profile_pic = details['profile_pic_url_hd'].replace('?','----').replace('&','____')
+                        time.sleep(1)
+                        display_url = details['edge_owner_to_timeline_media']['edges']
+                        print(description,followers,Following,profile_pic)
                         # User +3 Photos
+                        photo3 = []
                         count = 0
-                        post = profile.__dict__['_node']['edge_owner_to_timeline_media']['edges']
-                        for i in post:
-                            Photo3.append(i['node']['display_url'].replace('?','----').replace('&','____').strip())
-                            print(i['node']['display_url'].replace('?','----').replace('&','____'))
-                            count = count+1
+                        for i in display_url:
+                            photo3.append(i['node']['display_url'].replace('?','----').replace('&','____'))
+                            print(i['node']['display_url'])
+                            count = count + 1
+                            test.append(count)
                             if count == 4:
                                 print("Loop Break")
                                 break
-                        # Stored data in the database using an Api "https://beta.scouted.by/v1/importDeepScouting"
+                        time.sleep(1)
                         try:
-                            if len(Photo3) == 0:
-                                print('If Condition Private User')
-                                urls = ''
-                            else:
-                                print("Else Condition 4+ photos")
-                                try:
-                                    url = Photo3[0]
-                                except:
-                                    url = ''
-                                try:
-                                    url_ = Photo3[1]
-                                except:
-                                    url_ = ''
-                                try:   
-                                    url_s = Photo3[2]
-                                except:
-                                    url_s = ''
-                                try:
-                                    url_post = Photo3[3]
-                                except:
-                                    url_post = ''
-                                urls = str(url)+','+str(url_)+','+url_s+','+url_post
-                            Url = "https://beta.scouted.by/v1/insertDeepScouting?id=d0dd6e0cf620541fdd14527cc9a9813a&signature=d0dd6e0cf620541fdd14527cc9a9813a-"+str(Trace_id)+'-'+str(user_name)+'-'+"02a21adad3229c35f5bfc20ecbbb9ae1&followers="+str(followers)+"&following="+str(Following)+"&profile_photo="+str(profile_pic)+"&blocked=0&description="+str(description)+"&photos[]="+urls+"=&trace_id="+str(Trace_id)+"&instagram="+str(user_name)
-                            driver.get(Url)
+                            file_1 = photo3[0]
                         except:
-                            pass
+                            file_1 = ''
+                        try:
+                            file_2 = photo3[1]
+                        except:
+                            file_2 = ''
+                        try:
+                            file_3 = photo3[2]
+                        except:
+                            file_3 = ''
+                        try:
+                            file_4 = photo3[3]
+                        except:
+                            file_4 = ''
+
+                        Url = "https://beta.scouted.by/v1/insertDeepScouting?id=d0dd6e0cf620541fdd14527cc9a9813a&signature=d0dd6e0cf620541fdd14527cc9a9813a-"+str(Trace_id)+'-'+str(user_name)+'-'+"02a21adad3229c35f5bfc20ecbbb9ae1&followers="+str(followers)+"&following="+str(Following)+"&profile_photo="+str(profile_pic)+"&blocked=0&description="+str(description)+"&photos[]="+str(file_1)+"&photos[]="+str(file_2)+"&photos[]="+str(file_3)+"&photos[]="+str(file_4)+"=&trace_id="+str(Trace_id)+"&instagram="+str(user_name)
+                        f = requests.get(Url)
+                        print(f.text)
+                        print("Done")
                     except:
                         pass
-                    time.sleep(25)
-           
+                    
+                    if len(test) == 160 or len(test) == '100':
+                        time.sleep(2000)
+                    time.sleep(100)
             # Get_info function call        
             Get_info(list_of_user)
            
+
 # instagram account list
-account = [('scout_marie1','na888888'),('scout_marie2','na888888'),('scout_marie3','na888888')]
+account = [('scout_marie1','na888888'),('john20.21','puneet@6644'),('irissannevloik','kp48ffkn'),('polinakurtaa','kp48ffkn')]
 random.shuffle(account)    
 Insta_user = account[0][0]
 Insta_password = account[0][1]
@@ -158,4 +189,7 @@ with open('scoutinglist - Blad.csv', 'r') as file:
         scraped_users.append(row[0])
 Username = Login(Insta_user,Insta_password, scraped_users)
 Username.Infomation()
+
+
+
 
